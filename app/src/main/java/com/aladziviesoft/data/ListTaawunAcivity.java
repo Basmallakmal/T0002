dc98cf09bb15ded56f07f3867c627a81723ed128
basmallakmal <akmalfaseh18@gmail.com>
2018-12-17 16:46:29 +0700

    list data,update,delete

--- app/src/main/java/com/aladziviesoft/data/ListTaawunAcivity.java
+++ app/src/main/java/com/aladziviesoft/data/ListTaawunAcivity.java
@@ -1,13 +1,106 @@
 package com.aladziviesoft.data;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.aladziviesoft.data.adapter.DBAdapter;
 
 public class ListTaawunAcivity extends AppCompatActivity {
+    String[] daftar;
+    ListView ListView01;
+    Menu menu;
+    protected Cursor cursor;
+    DBAdapter dbcenter;
+    public static ListTaawunAcivity ma;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_taawun_acivity);
+
+        ma = this;
+        dbcenter = new DBAdapter(this);
+        RefreshList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite) {
+            Intent a = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(a);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void RefreshList() {
+        SQLiteDatabase db = dbcenter.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM data_pembayaran",null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc=0; cc < cursor.getCount(); cc++){
+            cursor.moveToPosition(cc);
+            daftar[cc] = cursor.getString(1).toString();
+        }
+        ListView01 = (ListView)findViewById(R.id.listView1);
+        ListView01.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
+        ListView01.setSelected(true);
+        ListView01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
+                final String selection = daftar[arg2]; //.getItemAtPosition(arg2).toString();
+                final CharSequence[] dialogitem = {"Lihat data", "Update data", "Hapus data"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListTaawunAcivity.this);
+                builder.setTitle("Pilihan");
+                builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch(item){
+                            case 0 :
+                                Intent i = new Intent(getApplicationContext(), DetailActivity.class);
+                                i.putExtra("nama_pembayar", selection);
+                                startActivity(i);
+                                break;
+                            case 1 :
+                                Intent in = new Intent(getApplicationContext(), UpdateActivity.class);
+                                in.putExtra("nama_pembayar", selection);
+                                startActivity(in);
+                                break;
+                            case 2 :
+                                SQLiteDatabase db = dbcenter.getWritableDatabase();
+                                db.execSQL("delete from data_pembayaran where nama_pembayar = '"+selection+"'");
+                                RefreshList();
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }});
+        ((ArrayAdapter)ListView01.getAdapter()).notifyDataSetInvalidated();
     }
 }
--- app/src/main/java/com/aladziviesoft/data/MainActivity.java
+++ app/src/main/java/com/aladziviesoft/data/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aladziviesoft.data;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,31 +34,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-    simpan = (Button) findViewById(R.id.simpan);
-    nama = (EditText) findViewById(R.id.nama);
-    jumlahuang = (EditText) findViewById(R.id.uang);
-    tanggal = (TextView) findViewById(R.id.tanggal);
-    rgrub = (RadioGroup) findViewById(R.id.rgrub);
+        simpan = (Button) findViewById(R.id.simpan);
+        nama = (EditText) findViewById(R.id.nama);
+        jumlahuang = (EditText) findViewById(R.id.uang);
+        tanggal = (TextView) findViewById(R.id.tanggal);
+        rgrub = (RadioGroup) findViewById(R.id.rgrub);
 
         Calendar c = Calendar.getInstance();
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String formattedDate = df.format(c.getTime());
         tanggal.setText(formattedDate);
 
-    koneksi = new DBCRUD(this);
-    koneksi.open();
+        koneksi = new DBCRUD(this);
+        koneksi.open();
     }
 
-
-    public void viewdata(View view)
-    {
-        String data = koneksi.getData();
-        Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu2, menu);
+        return true;
     }
 
-    public void actsimpan(View view)
-    {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite2) {
+            Intent a = new Intent(getApplicationContext(), ListTaawunAcivity.class);
+            startActivity(a);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void actsimpan(View view) {
         String snama = nama.getText().toString();
         String suang = jumlahuang.getText().toString();
         String stanggal = tanggal.getText().toString();
@@ -63,25 +80,22 @@ public class MainActivity extends AppCompatActivity {
         rbutton = (RadioButton) findViewById(selectedid);
         String srbutton = rbutton.getText().toString();
 
-        if(snama.isEmpty() || suang.isEmpty())
-        {
-            Toast.makeText(MainActivity.this,"Masukkan Nama dan Jumlah uang", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            long id = koneksi.insertData(snama,suang,stanggal,srbutton);
-            if(id<=0)
-            {
-                Toast.makeText(MainActivity.this,"Gagal", Toast.LENGTH_SHORT).show();
+        if (snama.isEmpty() || suang.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Masukkan Nama dan Jumlah uang", Toast.LENGTH_SHORT).show();
+        } else {
+            long id = koneksi.insertData(snama, suang, stanggal, srbutton);
+            if (id <= 0) {
+                Toast.makeText(MainActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
                 nama.setText("");
                 jumlahuang.setText("");
-            } else
-            {
-                Toast.makeText(MainActivity.this,"Berhasil", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Berhasil", Toast.LENGTH_SHORT).show();
                 nama.setText("");
                 jumlahuang.setText("");
             }
         }
+        ListTaawunAcivity.ma.RefreshList();
+        finish();
     }
 
 }
